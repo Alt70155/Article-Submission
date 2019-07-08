90495d62f1187de5c2eb32b5aa77f0630fe49315
chika <chika1086@gmail.com>
2019-07-08 09:35:52 +0900

    Add static pages controller test

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,21 @@
+require './test/test_helper.rb'
+
+class StaticPagesControllerTests < MiniTest::Test
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  def test_should_get_index
+    get '/'
+    assert last_response.ok?
+    assert last_response.body.include?('Knowledge of Programming')
+  end
+
+  def test_should_get_login
+    get '/login'
+    assert last_response.ok?
+    assert last_response.body.include?('ログイン')
+  end
+end

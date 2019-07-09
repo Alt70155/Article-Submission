7ff58eaa21f7297739acd6ed4d3f42147b2ccf24
chika <chika1086@gmail.com>
2019-07-09 19:25:41 +0900

    Fix test to validate the page

--- test/controllers/login_controller_test.rb
+++ test/controllers/login_controller_test.rb
@@ -4,17 +4,14 @@ class UsersControllerTests < MiniTest::Test
   def test_should_redirect_login_when_not_logged_in
     get '/create_article'
     follow_redirect!
-    assert last_response.body.include?('ログイン')
+    assert_equal '/login', last_request.path_info
   end
 
   def test_should_login_create_article_page_when_correct_user
     get '/login'
-    assert last_response.body.include?('ログイン')
-    post '/login', params = {
-      user_id:  'test',
-      password: 'password'
-    }
+    assert_equal '/login', last_request.path_info
+    log_in_as_test_user
     follow_redirect!
-    assert last_response.body.include?('Article Post')
+    assert_equal '/create_article', last_request.path_info
   end
 end

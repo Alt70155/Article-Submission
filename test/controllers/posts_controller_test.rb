3bf30c6cd5104d7ad8f053e04cb47fd0bbe6acd1
chika <chika1086@gmail.com>
2019-07-12 14:32:56 +0900

    Add post controllert test

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -20,17 +20,38 @@ class PostsControllerTest < MiniTest::Test
 
   def test_Should_redirect_if_csrf_token_is_wrong
     log_in_as_test_user
-    post '/article_post', params = {
-      csrf_token:  'overwrite',
-      category_id: 1,
-      title:       'Example Title',
-      body:        'Lorem ipsum...',
-      file:        Rack::Test::UploadedFile.new('test/sample.jpg', 'image/jpeg')
-    }
+    post_a_test_article(ex: { csrf_token: 'overwrite' })
     follow_redirect!
     assert_equal '/login', last_request.path_info
   end
-end
 
-# csrf対策のテスト
-# ファイルをpostしないといけない
+  def test_should_select_an_image_in_the_article_if_there_is_an_in_article_image_tag
+    log_in_as_test_user
+    post_count = Post.count
+    post_a_test_article(body: '![]()', ex: { 'article_img_files[]': Rack::Test::UploadedFile.new('test/sample.jpg', 'image/jpeg') })
+    assert_equal post_count + 1, Post.count
+  end
+
+  def test_should_not_post_if_you_have_an_image_tag_but_no_image
+    log_in_as_test_user
+    post_count = Post.count
+    post_a_test_article(body: '![]()')
+    assert_equal post_count, Post.count
+  end
+
+  def test_should_not_post_if_no_image_tag_but_there_is_an_image
+    log_in_as_test_user
+    post_count = Post.count
+    post_a_test_article(ex: { 'article_img_files[]': Rack::Test::UploadedFile.new('test/sample.jpg', 'image/jpeg') })
+    assert_equal post_count, Post.count
+  end
+
+  # 記事投稿に失敗し、その後記事プレビューに失敗するとログイン画面にリダイレクトされるバグを再現したテスト
+  def test_if_you_fail_to_post_an_article_and_then_fail_to_preview_an_article_you_will_not_be_redirected_to_the_login_screen
+    log_in_as_test_user
+    post '/article_post'
+    assert last_response.body.include?('Article Post')
+    post '/article_prev'
+    assert last_response.body.include?('Article Post')
+  end
+end

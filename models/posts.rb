2740f711d3458a8e8a7c5afc91fbedfb03716e3a
chika <chika1086@gmail.com>
2019-10-11 15:24:07 +0900

    記事本文のバリデーションを変更

--- models/posts.rb
+++ models/posts.rb
@@ -5,7 +5,7 @@ class Post < ActiveRecord::Base
 
   validates :category_id, presence: true, numericality: { only_integer: true }
   validates :title, length: { in: 1..75 }
-  validates :body,  length: { in: 1..20000 }
+  validates :body,  length: { in: 1..50000 }
   validates :top_picture, presence: true,
             format: { with: ENABLE_EXTENSION_REGEXP, message: 'is only jpg, jpeg, png' }
   # validates_withヘルパーは、バリデーション専用の別クラスにレコードを渡す
